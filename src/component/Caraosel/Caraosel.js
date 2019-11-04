a9bc2abc25677c568d1476c2b9166a45b22ff53a
AnshikaPranzal <54029542+AnshikaPranzal@users.noreply.github.com>
2019-11-04 19:47:41 +0530

    Update Caraosel.js

--- src/component/Caraosel/Caraosel.js
+++ src/component/Caraosel/Caraosel.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-import c from './Caraosel.css';
+import h from './Register.css';
  
-
-const ca = () => (
-    
-    <div >
-    <img src='.../assets/w1.jpg' alt="ft"></img>
-  </div>
   
+const hl = lo => (
+   
+   <div className={h.bdy}>
+   <div className={h.hj} >
+      
+        
+        
+          
+        
+            <h3 className={h.ik}><strong>Sign up to our Newsletter</strong></h3>
+            
+            <p>If you sign up for our newsletter,you will never miss a single news or updates from our website.So, don't waste a single moment.Sign Up, now.</p>
+            <div className={h.jk}>
+            <input className={h.o} type="text" value="  Your email Id here"></input>
+            <input className={h.io} type="button" value="Submit"></input>
+          </div>
+      
+     
+    </div>
+    </div>
+     
 );
 
-export default ca;
+export default hl;
