8975456e0b664b49aaf545472014dd69445795c3
AnshikaPranzal <54029542+AnshikaPranzal@users.noreply.github.com>
2019-11-04 19:31:10 +0530

    Update About.js

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -8,7 +8,7 @@ const ab = lo => (
        <h1>An <span>overview</span> <br></br>of your skills.</h1>
       <p>A key component of your transformation, your digital strategy requires special attention. We provide you with the support you need all during your project. Through regular workshops, brainstormings and idea sharing, we help grow your initial idea into a cutting-edge digital solution.
      <br></br> <br></br>A key component of your transformation, your digital strategy requires special attention. We provide you with the support you need all during your project. Through regular workshops, brainstormings and idea sharing, we help grow your initial idea into a cutting-edge digital solution.</p>
-   
+  
 </div>
      
 );
